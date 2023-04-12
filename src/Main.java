a565512be8127a348dacf0822f56b274631c5afd
alekanic <koirallabublik@gmail.com>
2023-04-12 17:34:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 13_676;
+        int rub = 20;
+        int bonus = ticket / rub;
+
+        System.out.println(bonus);
+
+    }
+}
